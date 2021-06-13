cd918291deb979d526253ae6c816eb21e3054190
Saori Nakagami <someyasaori@gmail.com>
2021-06-13 16:09:43 +0900

    6/13 16:00

--- input.php
+++ input.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-
     <title>アンケートフォーム</title>
 </head>
 <body>
@@ -13,10 +12,10 @@
     <h1>電気代に関するアンケート</h1>
     <form method ="post" action="write.php">
     <div class="basic">
-        <p>お名前： <input type="text" name ="name" id ="name" size="20"></p>
+        <p>お名前： <input type="text" name ="name" id ="name" size="15"></p>
         <p>メールアドレス： <input type="text" name ="email" id ="email" size="20"></p>
     </div>   
-    <div class="question">
+    <div class="basic">
         <p>家族構成
             <select name="family" id="family">
             <option value="1人">1人</option>
@@ -31,27 +30,39 @@
             <option value="3,000">～3,000円</option>
             <option value="4,000">3,001～5,000円</option>
             <option value="6,000">5,001～7,000円</option>
-            <option value="8,000">7,001～9,000円</option>
+            <op6/13 16:00tion value="8,000">7,001～9,000円</op6>
             <option value="10,000">9,000円以上</option></p>
-        <button id="button"><input type="submit" value="送信" id="submit"></button>
     </div>
+    <br>
+    <div class="submit_button">
+        <input type="submit" value="送信" id="submit"></button>
+    </div>
+
     </form>
-
+<div class="result">
     <a href="read.php">これまでの結果を表示</a>
+</div>
+<footer>
+    <img src="img/aisatsu_dog.png" alt="dog" width ="33" height="40">
+    ご協力ありがとうございました
+    <img src="img/aisatsu_dog.png" alt="dog" width ="33" height="40">
+</footer>
 </main>
-   
+
+
+
 <!-- JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
 <!-- JQuery -->
 
-<script>
+<!-- <script>
     $("#button").on("click",function(){
     $("#name").html("");
     $("#email").html("");
     $("#family").html("");
     $("#bill").html("");
 });
-</script>
+</script> -->
 
 </body>
 </html>
